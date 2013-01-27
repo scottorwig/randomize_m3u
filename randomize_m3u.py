33faa7077952f2d2196439bb59a0dc8c9866b627
Scott Orwig <scott@orwig.us>
2013-01-27 14:06:46 -0500

    __init__ working

--- randomize_m3u.py
+++ randomize_m3u.py
@@ -11,9 +11,7 @@ import random
 import subprocess
 import sqlite3
 import sys
-import time
-
-
+import datetime
 
 CONFIGURATION_FILE = 'randomize_m3u.cfg'
 DATABASE_FILE = 'randomize_m3u.db'
@@ -24,7 +22,7 @@ config = ConfigParser.ConfigParser()
 if config.read(CONFIGURATION_FILE):
     print 'Reading config file at {0}'.format(CONFIGURATION_FILE)
     PATH_TO_VLC = config.get('vlc','path')
-    ITUNES_LIBRARY_PATH = config.get('itunes','library_path')
+    ORIGINAL_ITUNES_MUSIC_DIRECTORY = config.get('itunes','original_itunes_music_directory')
     GMAIL_USER = config.get('gmail','user')
     GMAIL_PASSWORD = config.get('gmail','pwd')
     PROWL_ADDRESS = config.get('prowl','address')
@@ -34,31 +32,34 @@ else:
 
 if len(sys.argv) > 1:
     print sys.argv
-    source_playlist_path = sys.argv[1]
-    print 'Source playlist: {0}'.format(source_playlist_path)
-    playlist_minutes = int(sys.argv[2])
-    print 'Playlist will run for {0} minutes'.format(playlist_minutes)
-    print 'Sleeping for five seconds . . .'
-    time.sleep(5)
+    SOURCE_PLAYLIST_PATH = sys.argv[1]
+    PLAYLIST_MINUTES = int(sys.argv[2])
 
-class Playist_Randomizer():
+class PlayistRandomizer():
     """Class to generate a random playlist"""
     
-    def __init__(full_path_to_source_playist, number_of_minutes_in_new_playlist):
-        self.full_path_to_source_playist = full_path_to_source_playist
+    def __init__(self, full_path_to_source_playist, number_of_minutes_in_new_playlist, generated_playlist_name, original_itunes_music_directory):
+        """ Create an instance of the PlaylistRandomizer class """
+        self.source_playlist_path = full_path_to_source_playist
+        self.source_playlist_name = os.path.basename(full_path_to_source_playist)
+        self.music_directory = os.path.dirname(full_path_to_source_playist)
+        self.original_itunes_music_directory = original_itunes_music_directory
         self.number_of_minutes_in_new_playlist = number_of_minutes_in_new_playlist
+        self.generated_playlist_name = generated_playlist_name
+        self.generated_playlist_path = os.path.join(self.music_directory,generated_playlist_name)
+        now = datetime.datetime.now()
+        self.now_iso = now.strftime("%Y-%m-%d_%H-%M")
+        self.file_reader = open(full_path_to_source_playist, 'r')
+        self.line_at_a_time = self.file_reader.readlines()
+        self.file_writer = open(self.generated_playlist_path, 'w')        
 
-    def Generate_Playlist(source_playlist_path, GENERATED_PLAYLIST_FILENAME, playlist_minutes, DATABASE_FILE, GMAIL_USER, PROWL_ADDRESS, GMAIL_PASSWORD, PATH_TO_VLC):
-        source_directory = os.path.dirname(source_playlist_path)
+    def generate_playlist(self, source_playlist_path, GENERATED_PLAYLIST_FILENAME, playlist_minutes, DATABASE_FILE, GMAIL_USER, PROWL_ADDRESS, GMAIL_PASSWORD, PATH_TO_VLC):
+        """ """
         source_playlist_name = os.path.basename(source_playlist_path)
         today = datetime.datetime.now()
-        now_iso = today.strftime("%Y-%m-%d_%H-%M")
-        generated_playlist_path = os.path.join(source_directory,GENERATED_PLAYLIST_FILENAME)
         print 'Playlist of {0} minutes will be generated from {1} at {2}'.format(playlist_minutes, source_playlist_path, generated_playlist_path)
     
-        file_reader = open(source_playlist_path, 'r')
-        line_at_a_time = file_reader.readlines()
-        file_writer = open(generated_playlist_path, 'w')
+        
     
         song_list = []
         for m3u_line in line_at_a_time:
@@ -122,4 +123,9 @@ class Playist_Randomizer():
         print 'player args are {0}'.format(player_args)
         p = subprocess.Popen(player_args)
 
-Generate_Playlist(os, source_playlist_path, datetime, GENERATED_PLAYLIST_FILENAME, playlist_minutes, random, sqlite3, DATABASE_FILE, GMAIL_USER, PROWL_ADDRESS, GMAIL_PASSWORD, gmailer, PATH_TO_VLC, subprocess)
+if __name__ == "__main__":
+    randomizer = PlayistRandomizer(SOURCE_PLAYLIST_PATH, PLAYLIST_MINUTES, GENERATED_PLAYLIST_FILENAME, ORIGINAL_ITUNES_MUSIC_DIRECTORY)
+    print randomizer
+    print randomizer.source_playlist_path
+    print randomizer.source_playlist_name
+    print randomizer.music_directory
